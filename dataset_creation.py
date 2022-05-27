36b989df599d94f2b69e975ecea6b20487959f92
RiahiFedi <69672015+RiahiFedi@users.noreply.github.com>
2022-05-27 15:20:31 +0100

    Update dataset_creation.py

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen as req_url
 import json
+import string
+import webbrowser
 import pandas as pd
 import time
 
@@ -10,22 +12,33 @@ def warframe(search):
     data = main_url.read()
     parsed = json.loads(data)
     if len(parsed['payload']['orders'])>0:
-        parsed_data = list([parsed['payload']['orders'][i]['platinum'],parsed['payload']['orders'][i]['user']['status']] for i in range(len(parsed['payload']['orders'])))
+        parsed_data = list([parsed['payload']['orders'][i]['platinum'],
+                            parsed['payload']['orders'][i]['user']['status'],
+                            parsed['payload']['orders'][i]['quantity']
+                            ] for i in range(len(parsed['payload']['orders'])))
         temp_ = []
         for j in range(len(parsed_data)):
             if parsed_data[j][1]!='offline':
                 temp_.append(parsed_data[j])
         if len(temp_)>0:
             minimal_price_ = min(list(temp_[k][0] for k in range(len(temp_)))) 
-            mode_ = 'alive'
+            status = 'Online'
+            t_ = list(temp_[k][0] for k in range(len(temp_)))
+            quantity = temp_[t_.index(minimal_price_)][2]
         else:
-            minimal_price_ = min(list(parsed['payload']['orders'][i]['platinum'] for i in range(len(parsed['payload']['orders']))))
-            mode_ = 'Dead'  
+            minimal_price_ = min(list(parsed_data[p][0] for p in range(len(parsed_data))))
+            status = 'Offline'
+            #minimal_price_ = min(list(parsed['payload']['orders'][i]['platinum'] for i in range(len(parsed['payload']['orders']))))    
+            t_ = list(parsed_data[p][2] for p in range(len(parsed_data)))
+            #t_ = list(list(parsed['payload']['orders'][i]['quantity'] for i in range(len(parsed['payload']['orders']))))
+            quantity = t_[list(parsed_data[p][0] for p in range(len(parsed_data))).index(minimal_price_)]
         #date_time = parsed['payload']['orders'][-1]['last_update']
     else:
         minimal_price_ = '0'
-        mode_ = 'Not Found'
-    return minimal_price_, mode_
+        status = 'Not found'
+        quantity = '0'
+        
+    return minimal_price_, status, quantity
 
 def gen_data(update=True):
     main_url = req_url('https://api.warframe.market/v1/items')
@@ -56,14 +69,15 @@ def prices(update = True):
     mode_ = []
     for i in range(len(df['url_name'].tolist())):
         time.sleep(0.4)
-        price_temp, mode_ = warframe(df['url_name'].tolist()[i])
+        price_temp, status, quantity = warframe(df['url_name'].tolist()[i])
         prices_.append(price_temp)
         #last_update.append(date_temp)
         print(df['url_name'].tolist()[i])
         print('price: ' + str(price_temp) + " nbr: "+ str(i) )
         #print(date_temp)
     df['prices'] =prices_
-    df['Mode'] = mode_
+    df['Mode'] = status
+    df['Quantity'] = quantity
     df.info()
     df.to_excel('Item_data.xlsx')
     
